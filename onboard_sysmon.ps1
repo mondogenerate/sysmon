a8a84b4540017ba9b42505102a4bf7686f346b21
r.Lee <65114647+mellonaut@users.noreply.github.com>
2022-04-25 17:53:13 -0400

    Set-EP

--- onboard_sysmon.ps1
+++ onboard_sysmon.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -Bypass
 mkdir "C:\sysmon";
 Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/sysmon.zip" -OutFile "C:\sysmon\sysmon.zip";
 Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
