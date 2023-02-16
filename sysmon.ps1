4f71841c0517abe5f98fc470fc3d16747f86708a
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-02-16 12:34:07 -0500

    Cleaned up old crap

--- sysmon.ps1
+++
@@ -1,8 +0,0 @@
-mkdir "C:\sysmon";
-Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/sysmon.zip" -OutFile "C:\sysmon\sysmon.zip";
-Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
-cd "c:\sysmon";
-c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmonconfig.xml
-
-# Looks sexy:
-# powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mellonaut/sysmon/master/sysmon.ps1'))"
