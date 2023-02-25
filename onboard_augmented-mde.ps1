723de8e6adecf666e6b8cb2f937d5614cb8578a0
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-02-25 14:07:23 -0500

    Scripts for different configs

---
+++ onboard_augmented-mde.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy -Bypass
+mkdir "C:\sysmon";
+Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/sysmon.zip" -OutFile "C:\sysmon\sysmon.zip";
+Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
+cd "c:\sysmon";
+c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmonconfig-mde-augment.xml
--- onboard_swift.ps1
+++ onboard_swift.ps1
@@ -4,6 +4,3 @@ Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/sysmon.zip"
 Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
 c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmonconfig.xml
-
-# Looks sexy:
-# powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mellonaut/sysmon/master/sysmon.ps1'))"
