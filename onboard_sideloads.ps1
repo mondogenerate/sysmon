a7a3acac87ef2d95fc08eb3409a5d94f6a9e9423
mondogenerate <rmattingly@anchorconst.com>
2023-09-29 13:13:15 -0400

    err

--- onboard_sideloads.ps1
+++ onboard_sideloads.ps1
@@ -1,10 +1,10 @@
 # One-Liner for Deployment:
-# iwr https://raw.githubusercontent.com/mondogenerate/sysmon/main/onboard_mde.ps1 -UseBasic  | iex
+# iwr https://raw.githubusercontent.com/mondogenerate/sysmon/main/onboard_sideloads.ps1 -UseBasic  | iex
 Write-Output "Triggered new script"
 powershell Set-ExecutionPolicy Bypass
 if(Test-Path C:\sysmon){ Remove-Item  -r c:\sysmon -Force}
 mkdir "C:\sysmon";
-$fileName = "onboard_mde"
+$fileName = "onboard_sideloads"
 Invoke-WebRequest -Uri "https://github.com/mondogenerate/sysmon/raw/main/$fileName.zip" -OutFile "C:\sysmon\$fileName.zip" -UseBasicParsing;
 Expand-Archive "c:\sysmon\$fileName.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
--- onboard_swift.ps1
+++ onboard_swift.ps1
@@ -1,12 +1,14 @@
-Set-ExecutionPolicy -Bypass
+# One-Liner for Deployment:
+# iwr https://raw.githubusercontent.com/mondogenerate/sysmon/main/onboard_swift.ps1 -UseBasic  | iex
+Write-Output "Triggered new script"
+powershell Set-ExecutionPolicy Bypass
+if(Test-Path C:\sysmon){ Remove-Item  -r c:\sysmon -Force}
 mkdir "C:\sysmon";
-Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/sysmon.zip" -OutFile "C:\sysmon\sysmon.zip";
-Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
+$fileName = "onboard_swift"
+Invoke-WebRequest -Uri "https://github.com/mondogenerate/sysmon/raw/main/$fileName.zip" -OutFile "C:\sysmon\$fileName.zip" -UseBasicParsing;
+Expand-Archive "c:\sysmon\$fileName.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
-c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmon-swift.xml
-
-
-.\WindowsDefenderATPLocalOnboardingScript.cmd
-.\GetAutoPilotInfo.cmd
-
-.\GetEvents.ps1
+c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\$fileName.xml
+Remove-Item c:\sysmon\$fileName.xml
+Remove-Item c:\sysmon\$fileName.zip
+Remove-Item c:\sysmon\$fileName.ps1
