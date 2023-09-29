556131ec7d491d8cd3ae87c994895706e44683e2
mondogenerate <rmattingly@anchorconst.com>
2023-09-29 07:45:29 -0400

    new MDE way, deletes leftover

--- onboard_mde.ps1
+++ onboard_mde.ps1
@@ -1,6 +1,10 @@
 Set-ExecutionPolicy -Bypass
 mkdir "C:\sysmon";
-Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/sysmon.zip" -OutFile "C:\sysmon\sysmon.zip";
-Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
+$fileName = "onboard_mde"
+Invoke-WebRequest -Uri "https://github.com/mellonaut/sysmon/raw/main/$fileName.zip" -OutFile "C:\sysmon\$fileName.zip" -UseBasicParsing;
+Expand-Archive "c:\sysmon\$fileName.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
-c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmonconfig-mde-augment.xml
+c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\$fileName.xml
+Remove-Item c:\sysmon\$fileName.xml
+Remove-Item c:\sysmon\$fileName.zip
+Remove-Item c:\sysmon\$fileName.ps1
