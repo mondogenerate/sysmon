74f414047ac1e510a8fc0d1f96753f7124ad616e
mondogenerate <rmattingly@anchorconst.com>
2023-09-29 13:01:47 -0400

    err

--- onboard_mde.ps1
+++ onboard_mde.ps1
@@ -1,11 +1,12 @@
 Write-Output "Triggered new script"
 powershell Set-ExecutionPolicy Bypass
+if(Test-Path C:\sysmon){ Remove-Item  -r c:\sysmon -Force}
 mkdir "C:\sysmon";
 $fileName = "onboard_mde"
 Invoke-WebRequest -Uri "https://github.com/mondogenerate/sysmon/raw/main/$fileName.zip" -OutFile "C:\sysmon\$fileName.zip" -UseBasicParsing;
 Expand-Archive "c:\sysmon\$fileName.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
 c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\$fileName.xml
-Remove-Item c:\sysmon\$fileName.xml
-Remove-Item c:\sysmon\$fileName.zip
-Remove-Item c:\sysmon\$fileName.ps1
+# Remove-Item c:\sysmon\$fileName.xml
+# Remove-Item c:\sysmon\$fileName.zip
+# Remove-Item c:\sysmon\$fileName.ps1
