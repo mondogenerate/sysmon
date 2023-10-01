66906867d5d142750ff8c8af798eabf782ce878c
mondogenerate <137521730+mondogenerate@users.noreply.github.com>
2023-10-01 05:22:38 -0400

    Update change_mde.ps1
    Console output

--- change_mde.ps1
+++ change_mde.ps1
@@ -9,6 +9,7 @@ Invoke-WebRequest -Uri "https://github.com/mondogenerate/sysmon/raw/main/$fileNa
 Expand-Archive "c:\sysmon\$fileName.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
 c:\sysmon\sysmon.exe -acceptEula -c c:\sysmon\$fileName.xml
+Write-Output "Cleaning up artifacts.."
 Remove-Item c:\sysmon\$fileName.xml
 Remove-Item c:\sysmon\$fileName.zip
 Remove-Item c:\sysmon\$fileName.ps1
