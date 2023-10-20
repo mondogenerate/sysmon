cd3ee5b75a43614cd9df29dea2980fccdc5550e4
mondogenerate <rmattingly@anchorconst.com>
2023-10-20 11:33:25 -0400

    modular

--- onboard_modular.ps1
+++ onboard_modular.ps1
@@ -1,21 +1,17 @@
 # One-Liner for Deployment:
-# iwr https://raw.githubusercontent.com/mondogenerate/sysmon/main/change_mde.ps1 -UseBasic  | iex
+# iwr https://raw.githubusercontent.com/mondogenerate/sysmon/main/onboard_modular.ps1 -UseBasic  | iex
 $tag = "modular"
 $fileName = "onboard_$tag"
+
 Write-Output "Triggered new script"
 powershell Set-ExecutionPolicy Bypass
-
-
-
 if(Test-Path C:\sysmon){ Remove-Item  -r c:\sysmon -Force}
 mkdir "C:\sysmon";
+
 Invoke-WebRequest -Uri "https://github.com/mondogenerate/sysmon/raw/main/$fileName.zip" -OutFile "C:\sysmon\$fileName.zip" -UseBasicParsing;
 Expand-Archive "c:\sysmon\$fileName.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
-c:\sysmon\sysmon.exe -acceptEula -c c:\sysmon\$fileName.xml
-Write-Output "Cleaning up artifacts.."
+c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\$fileName.xml
 Remove-Item c:\sysmon\$fileName.xml
 Remove-Item c:\sysmon\$fileName.zip
 Remove-Item c:\sysmon\$fileName.ps1
-Remove-Item C:\Sysmon\*.zip
-Remove-Item C:\Sysmon\*.xml
