01c897bf963e12bd6b395f716b8418869ec7841d
mondogenerate <137521730+mondogenerate@users.noreply.github.com>
2024-01-22 08:10:48 -0500

    Update PSWindowsUpdate_UpdateAll.ps1

--- Tools/PSWindowsUpdate_UpdateAll.ps1
+++ Tools/PSWindowsUpdate_UpdateAll.ps1
@@ -1,5 +1,6 @@
 # Import PSWindowsUpdate module
-Import-Module PSWindowsUpdate
+Install-Module PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate -Force
 
 # Get and install updates
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot
